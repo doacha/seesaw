87a9387edc443b2b42555cf431757897bd4b864c
박시원 <tldnjs324@naver.com>
2023-09-04 16:04:00 +0900

    :sparkles:Feat: Add file structure

---
+++ back/seesaw/src/main/java/com/doacha/seesaw/SeesawApplication.java
@@ -0,0 +1,13 @@
+package com.doacha.seesaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeesawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeesawApplication.class, args);
+    }
+
+}
