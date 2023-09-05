91cd6d1aa056c9d3494f44c4ed66e81a31826415
Jiwon <kimjw3928@naver.com>
2023-09-05 14:14:54 +0900

    :sparkles: Feat : #57 entity생성, Swagger 설정
    - Group, UserGroup, Chat, Category 엔티티 생성
    - Swagger 설정
    - Swagger 동작을 위한 SecurityConfig 추가
    - GroupService, GroupRepository, GroupController 생성

---
+++ back/seesaw/src/main/java/com/doacha/seesaw/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.doacha.seesaw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        // 접속 혀용할 URL
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        // 나머지 요청은 인증 필요
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/entity/Category.java
@@ -0,0 +1,31 @@
+package com.doacha.seesaw.model.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Category")
+@Schema(description = "카테고리")
+public class Category {
+
+    @Id
+    @Column(name = "category_id", nullable = false)
+    @Schema(description = "카테고리 아이디", example = "0", required = true)
+    private String categoryId;
+
+    @Column(name = "category_name", nullable = false)
+    @Schema(description = "카테고리 이름", example = "전체", required = true)
+    private String categoryName;
+
+}
