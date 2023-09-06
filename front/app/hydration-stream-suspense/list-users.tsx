70abb457dd28da0dedf7f5efc7ebe534c1a2193a
jay <jjwoong1733@gmail.com>
2023-09-06 16:59:15 +0900

    react-query for app router 추가 - 예시코드 추가 - streamed hydration 방식 - prefetching using initial data / prefetching using hydration 방식도 있음 - 각자 학습하길 바람.

---
+++ front/app/hydration-stream-suspense/list-users.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { User } from "../types";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+async function getUsers() {
+  return (await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  )) as User[];
+}
+
+export default function ListUsers() {
+  const [count, setCount] = React.useState(0);
+  const { data } = useQuery<User[]>({
+    queryKey: ["stream-hydrate-users"],
+    queryFn: () => getUsers(),
+    suspense: true,
+    staleTime: 5 * 1000,
+  });
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <>
+      <p>{count}</p>
+      {
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gap: 20,
+          }}
+        >
+          {data?.map((user) => (
+            <div
+              key={user.id}
+              style={{ border: "1px solid #ccc", textAlign: "center" }}
+            >
+              <img
+                src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                alt={user.name}
+                style={{ width: 180, height: 180 }}
+              />
+              <h3>{user.name}</h3>
+            </div>
+          ))}
+        </div>
+      }
+    </>
+  );
+}
