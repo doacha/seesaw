892432d7d2cce75f8f265046fdff6a5530da823b
박시원 <tldnjs324@naver.com>
2023-09-07 17:50:29 +0900

    :sparkles: Feat: jwt security

--- back/seesaw/src/main/java/com/doacha/seesaw/jwt/MemberDetail.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/jwt/MemberDetail.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
 
+// Spring Security에 유저 정보를 관리하기 위함
 public class MemberDetail extends User {
     private final Member member;
 
