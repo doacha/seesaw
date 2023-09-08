d4205d1487c9a6f1af654f7df51a64dd0ea94b25
jay <jjwoong1733@gmail.com>
2023-09-08 16:41:05 +0900

    :sparkles: feat: Header, Navbar 추가 - Header title, backButton, plusButton 을 props로 받아서 구성 - Navbar 는 NavbarButton의 조합으로 구성. 클릭시 primary 색상으로 변환

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
-import Card from './components/card'
+import Card from './components/Card'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 const Entrance = () => {
   return (
     <div className="h-screen w-screen">
-      <Card title={'내 통계'} />
+      <Header title={'타이틀'} backButton plusButton />
+      {/* <Card title={'내 통계'} /> */}
+      <Navbar />
     </div>
   )
 }
