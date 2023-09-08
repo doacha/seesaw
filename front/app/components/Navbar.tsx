fe37e1ba7b43169948ff64ec9d2efde7667cb01d
jay <jjwoong1733@gmail.com>
2023-09-08 17:20:25 +0900

    :bug: Fix: 대소문자 관련 git 에러 수정 - 수동으로 파일 대소문자 수정시 git에서 인식 불가능 - 'git mv 기존파일명 새로운파일명'으로 수정해야함

---
+++ front/app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useState } from 'react'
+import NavbarButton from './NavbarButton'
+
+const Navbar = () => {
+  //add 버그 있음.
+  const [currentTab, setCurrentTab] = useState<string>('wallet')
+
+  const onTabClick = (value: string) => {
+    console.log(value)
+    setCurrentTab(value)
+  }
+
+  return (
+    <div className="btm-nav">
+      <NavbarButton
+        icon="wallet"
+        activated={currentTab === 'wallet' ? true : false}
+        onTabClick={() => onTabClick('wallet')}
+      />
+      <NavbarButton
+        icon="flag"
+        activated={currentTab === 'flag' ? true : false}
+        onTabClick={() => onTabClick('flag')}
+      />
+      <NavbarButton
+        icon="user"
+        activated={currentTab === 'user' ? true : false}
+        onTabClick={() => onTabClick('user')}
+      />
+    </div>
+  )
+}
+
+export default Navbar
