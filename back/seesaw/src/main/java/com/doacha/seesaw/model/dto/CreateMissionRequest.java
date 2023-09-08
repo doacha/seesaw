4ffb9e3c558d83a9ecb767000b92a2b70884d6ce
iamjione <kimjw3928@gmail.com>
2023-09-08 15:38:43 +0900

    :sparkles: Feat: Record API, Mission API 구현
    - Record CRUD 구현
    - MemberMissionRepository 만들기 위해 MemberMissionId DTO 생성
    - Mission 생성/참여/탈퇴 시 MemberMission 테이블 update
    
    - Mission 탈퇴시 시작일 이전인지, 미션 호스트인지 체크하는 부분
      추가해야함

---
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/dto/CreateMissionRequest.java
@@ -0,0 +1,61 @@
+package com.doacha.seesaw.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "미션 생성 request")
+public class CreateMissionRequest {
+
+    @Schema(description = "미션 제목", example = "일주일동안 5만원 쓰기", required = true)
+    private String missionTitle;
+
+    @Schema(description = "현재 인원수", example = "1", required = true)
+    private int missionMemberCount;
+
+    @Schema(description = "총 모집 인원수", example = "6", required = true)
+    private int missionMaxCount;
+
+    @Schema(description = "미션 이미지 Url", required = false)
+    private String missionImgUrl;
+
+    @Schema(description = "미션 소개글", example = "일주일동안 5만원쓰기 같이 하실 분!!", required = true)
+    private String missionPurpose;
+
+    @Schema(description = "최소 예치금", example = "30000", required = true)
+    private int missionMinDeposit;
+
+    @Schema(description = "미션 공개 여부", example = "true", required = true)
+    private boolean missionIsPublic;
+
+    @Schema(description = "미션 금액", example = "50000", required = true)
+    private int missionLimit;
+
+    @Schema(description = "미션 횟수", example = "4", required = true)
+    private int missionPeriod;
+
+    @Schema(description = "미션 주기", example = "7", required = true)
+    private int missionCycle;
+
+    @Schema(description = "미션 시작일", example = "2023-09-11", required = true)
+    private Date missionStartDate;
+
+    @Schema(description = "미션 그룹장 이메일", example = "doacha@seesaw.com", required = true)
+    private String missionHostEmail;
+
+    @Schema(description = "미션 카테고리 아이디", example = "0", required = true)
+    private long categoryId;
+
+    @Schema(description = "예치금", example = "40000", required = true)
+    private int memberMissionDeposit;
+
+    @Schema(description = "결제 번호", example = "T1234567890123456789", required = true)
+    private String memberMissionTnum;
+}
--- back/seesaw/src/main/java/com/doacha/seesaw/repository/MissionRepository.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/repository/MissionRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, String> { //JpaRepository<Entity클래스, PK타입>
-//    Page<MissionListResponse> findByMissionIsPublicOrderByMissionCreationTimeDesc(int groupIsPublic, Pageable pageable);
-//    Page<MissionListResponse> findByMissionIsPublicAndMissionTitleLikeOrderByMissionCreationTimeDesc(int groupIsPublic, String Keyword, Pageable pageable);
 
-    // group_id, group_title, group_member_count, group_max_count, group_img_url, group_min_deposit만 가져와야함
-    Page<MissionListResponse> findByMissionIsPublic(int groupIsPublic, Pageable pageable);
-    Page<MissionListResponse> findByMissionIsPublicAndMissionTitleLike(int groupIsPublic, String Keyword, Pageable pageable);
+    @Query("SELECT new com.doacha.seesaw.model.dto.MissionListResponse(m.missionId, m.missionTitle, m.missionMemberCount, m.missionMaxCount, m.missionImgUrl, m.missionMinDeposit) FROM Mission m WHERE m.missionIsPublic = true")
+    Page<MissionListResponse> findAllByMissionIsPublic(Pageable pageable);
+    @Query("SELECT new com.doacha.seesaw.model.dto.MissionListResponse(m.missionId, m.missionTitle, m.missionMemberCount, m.missionMaxCount, m.missionImgUrl, m.missionMinDeposit) FROM Mission m WHERE m.missionIsPublic = true AND LOWER(m.missionTitle) LIKE %:keyword%")
+    Page<MissionListResponse> findAllByMissionIsPublicAndMissionTitleLike(@Param("keyword") String keyword, Pageable pageable);
+
 
 }
