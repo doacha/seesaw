2779560571f276a0b5c7f4423e87b5127274803d
bongjun <funcodezune@gmail.com>
2023-09-08 17:53:53 +0900

    :sparkles: Feat:#7/공통컴포넌트제작-input - input 제작중

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,21 @@
 import Link from 'next/link'
 import Card from './components/card'
+import Input from './components/Input'
+import Button from './components/Button'
 
 const Entrance = () => {
+  const onClick = () => {}
+  const submitButton = <Button color="primary" label="test" size="xl" />
   return (
     <div className="h-screen w-screen">
       <Card title={'내 통계'} />
+      <Input
+        label="text"
+        type="text"
+        placeholder="tttest"
+        interval="15"
+        submitButton={submitButton}
+      />
     </div>
   )
 }
