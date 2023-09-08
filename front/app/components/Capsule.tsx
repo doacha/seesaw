f804445b7726d9885fd5eea7b64e31d916870ad7
bongjun <funcodezune@gmail.com>
2023-09-08 15:13:57 +0900

    :sparkles: Feat: #52/공통컴포넌트제작-캡슐 - 캡슐 공통 컴포넌트 구현 완료 - 필수 props : 배경색, 글자색 (tailwindCSS 형식) - 선택 props : isSmall, isHasBorder (미션 파트에서 사용)                onClick, children

--- front/app/components/Capsule.tsx
+++ front/app/components/Capsule.tsx
@@ -1,5 +1,36 @@
-const Capsule = () => {
-  return <span className="badge">test</span>
+// bcColor (배경색) 과 textColor(폰트색) 은 tailwindCSS 형식으로 필수적으로 입력해줍니다
+// isSmall : 작은 버튼 생성시 true 전달 (미션 파트에서 사용)
+// isHasBorder : border 존재할 경우 true 전달 (미션 파트에서 사용)
+// onClick : onClick 함수 전달
+
+const Capsule = ({
+  bgColor,
+  textColor,
+  isHasBorder,
+  isSmall,
+  onClick,
+  children,
+}: {
+  bgColor: string
+  textColor: string
+  isHasBorder?: boolean
+  isSmall?: boolean
+  onClick?: any
+  children: string
+}) => {
+  const tailwindBorder = isHasBorder ? 'border-[0.5px]' : 'border-0'
+  const tailwindFontSize = isSmall
+    ? 'text-[10px] h-[16px] py-[3px]'
+    : 'text-[14px] h-[30px] py-[8px]'
+
+  return (
+    <span
+      className={`badge ${bgColor} ${textColor} ${tailwindBorder} ${tailwindFontSize}`}
+      onClick={onClick}
+    >
+      {children}
+    </span>
+  )
 }
 
 export default Capsule
