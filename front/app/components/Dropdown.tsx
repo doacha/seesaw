7c9155f56a4bf1e37627f4f939812ac101de5519
jay <jjwoong1733@gmail.com>
2023-09-11 13:37:04 +0900

    :sparkles: Feat: 텍스트 버튼 제작 - innerText, textColor, onButtonClick  내려주면 됩니다. - div 로 감싸서 커스텀하여 사용하세요 - UserInfoCard 제작 중.

--- front/app/components/Dropdown.tsx
+++ front/app/components/Dropdown.tsx
@@ -16,27 +16,21 @@ const Dropdown = () => {
       <div className="collapse-title text-base pr-10 h-[40px]  min-h-0 pt-2">
         {currentSort}
       </div>
-      <div className="w-20 collapse-content">
-        <button
+      <div className=" collapse-content">
+        <p
           className="text-left"
           onClick={() => {
             onSortButtonClick('최신순')
           }}
         >
           최신순
-        </button>
-        <button
-          className="w-20 text-left"
-          onClick={() => onSortButtonClick('성공')}
-        >
+        </p>
+        <p className="text-left" onClick={() => onSortButtonClick('성공')}>
           성공
-        </button>
-        <button
-          className="w-20 text-left"
-          onClick={() => onSortButtonClick('실패')}
-        >
+        </p>
+        <p className="text-left" onClick={() => onSortButtonClick('실패')}>
           실패
-        </button>
+        </p>
       </div>
     </div>
   )
