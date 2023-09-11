6e4317b1d3d46a1ba5717832cd65b5e36086c7a4
박시원 <tldnjs324@naver.com>
2023-09-11 17:56:06 +0900

    :sparkles: Feat: modify api ing, jwt logout ing..

---
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/dto/user/ChangeInfoRequest.java
@@ -0,0 +1,29 @@
+package com.doacha.seesaw.model.dto.user;
+
+import com.doacha.seesaw.model.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChangeInfoRequest {
+
+    private final String memberEmail;
+
+    private final String memberPassword;
+
+    private final String memberNewPassword;
+
+    private final String memberName;
+
+    private final String memberNickname;
+
+    private final String memberBirth;
+
+    private final boolean memberGender;
+
+    private final String memberImgUrl;
+
+    private final String memberPhoneNumber;
+
+}
--- back/seesaw/src/main/java/com/doacha/seesaw/model/service/MemberService.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/service/MemberService.java
@@ -1,17 +1,19 @@
 package com.doacha.seesaw.model.service;
 
 import com.doacha.seesaw.exception.BadRequestException;
-import com.doacha.seesaw.model.dto.user.LoginRequest;
-import com.doacha.seesaw.model.dto.user.MyInfoResponse;
-import com.doacha.seesaw.model.dto.user.SignUpRequest;
-import com.doacha.seesaw.model.dto.user.MemberResponse;
+import com.doacha.seesaw.model.dto.user.*;
 import com.doacha.seesaw.model.entity.Member;
+import com.doacha.seesaw.model.entity.Mission;
+import com.doacha.seesaw.redis.RedisDao;
 import com.doacha.seesaw.repository.MemberRepository;
 //import jakarta.transaction.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     // TODO: 나중에 PasswordEncoder 다시 설정 해주기
     private final PasswordEncoder passwordEncoder;
+    private final RedisDao redisDao;
 
     // 회원가입
     @Transactional
@@ -59,6 +62,51 @@ public class MemberService {
         return MemberResponse.of(member);
     }
 
+    // 로그아웃
+    @Transactional
+    public void logout() {
+        //Token에서 로그인한 사용자 정보 get해 로그아웃 처리
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    }
+
+    // 회원 정보 수정
+    @Transactional(readOnly = true)
+    public MyInfoResponse changeInfo(ChangeInfoRequest changeInfoRequest) {
+//        Member member = memberRepository
+//                .findByMemberEmail(changeInfoRequest.getMemberEmail())
+//                .orElseThrow(() -> new BadRequestException("유저 정보가 없습니다."));
+
+        Optional<Member> member = memberRepository.findByMemberEmail(changeInfoRequest.getMemberEmail());
+
+        member.get().builder().memberEmail(member.get().getMemberEmail()).build();
+        member.get().builder().memberName(member.get().getMemberName()).build();
+        member.get().builder().memberBirth(member.get().getMemberBirth()).build();
+        member.get().builder().memberGender(member.get().isMemberGender()).build();
+        member.get().builder().memberNickname(member.get().getMemberNickname()).build();
+        // TODO : 빨리 만들어!!!
+
+        memberRepository.save(member.get());
+
+//        if(changeInfoRequest.getMemberNewPassword()!=null){ // 비번 새로 바꾸려고 하면
+//            if(!passwordEncoder.matches( changeInfoRequest.getMemberPassword(), member.getMemberPassword())){ // 비번 확인
+//                throw new BadRequestException("비밀번호를 확인하세요."); // 비번 다르면 익셉션
+//            }
+//            member.setMemberPassword(changeInfoRequest.getMemberNewPassword()); // 비번 일치하면 비번 바꿔줌
+//        }
+//        member.setMemberName(changeInfoRequest.getMemberName());
+//        member.setMemberEmail(changeInfoRequest.getMemberEmail());
+//        member.setMemberBirth(changeInfoRequest.getMemberBirth());
+//        member.setMemberGender(changeInfoRequest.isMemberGender());
+//        member.setMemberNickname(changeInfoRequest.getMemberNickname());
+//        member.setMemberImgUrl(changeInfoRequest.getMemberImgUrl());
+//        member.setMemberPhoneNumber(changeInfoRequest.getMemberPhoneNumber());
+
+
+
+
+        return MyInfoResponse.of(member.get());
+    }
     // 비밀번호 확인
     @Transactional(readOnly = true)
     public MyInfoResponse confirmPassword(LoginRequest loginRequest) {
