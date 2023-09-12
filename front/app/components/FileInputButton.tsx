5fc6df4c0ca1de54bc5a9c689784ed6178904d51
Hanna Kim <gksskrn0158@gmail.com>
2023-09-12 09:48:36 +0900

    :sparkles: Feat : #127 카메라 버튼 공통 컴포넌트 구현 - console.log로 사진의 url을 확인하도록 임시로 코드를 작성했습니다

--- front/app/components/FileInputButton.tsx
+++ front/app/components/FileInputButton.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
@@ -7,8 +8,9 @@ const FileInputButton = () => {
   const [image, setImage] = useState<string>()
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!!e.target.files) return
+    if (!e.target.files) return
     const file = e.target.files?.[0]
+    console.log(file)
     if (file) {
       const image = window.URL.createObjectURL(file)
       console.log(image)
@@ -18,7 +20,6 @@ const FileInputButton = () => {
 
   return (
     <div className="flex items-center justify-center w-full">
-      {/* 이미지 업로드 버튼 */}
       <label
         htmlFor="file"
         className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
@@ -39,12 +40,12 @@ const FileInputButton = () => {
           onChange={handleImageChange}
         />
       </label>
-      {/* 이미지 미리보기 */}
+      {/* 이미지 미리보기
       {image && (
         <div className="mt-4">
           <img src={image} alt="Uploaded" width="200" />
         </div>
-      )}
+      )} */}
     </div>
   )
 }
