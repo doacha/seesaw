232fe3dd5e367cf12e9d469be58e605878c7c1d7
Hanna Kim <gksskrn0158@gmail.com>
2023-09-13 16:05:30 +0900

    :sparkles: Feat : 회원가입페이지구현 - 비밀번호 이중 검사 - 이메일 및 비밀번호 유효성 검사 - useState 설정

--- front/app/regist/components/Email.tsx
+++ front/app/regist/components/Email.tsx
@@ -3,21 +3,32 @@ import Button from '@/app/components/Button'
 interface EmailProps {
   onClick: () => void
   onChange: (e: any) => void
+  isVaild: boolean
+  value: string
 }
 
-const Email = ({ onClick, onChange }: EmailProps) => {
+const Email = ({ onClick, onChange, isVaild, value }: EmailProps) => {
   return (
     <>
       <p className="font-scDreamExBold text-xs justify-start">이메일</p>
-      <div className="mt-2 mb-5 w-full">
+      <div className="mt-2 mb-7 w-full">
         <div className="grid gap-3 grid-cols-3">
           <div className="col-span-2">
             <Input
+              name="email"
               onChange={onChange}
               type="id"
               placeholder="이메일 입력"
               interval="0"
-            ></Input>
+              value={value}
+            />
+            <div className="relative">
+              {!isVaild && value.length > 0 && (
+                <p className="absolute top-0 left-0 mt-[2px] text-error text-xs">
+                  * 이메일 양식을 맞춰주세요!
+                </p>
+              )}
+            </div>
           </div>
           <div className="col-span-1 my-auto">
             <Button
@@ -25,7 +36,7 @@ const Email = ({ onClick, onChange }: EmailProps) => {
               label="인증하기"
               size="xs"
               onClick={onClick}
-            ></Button>
+            />
           </div>
         </div>
       </div>
