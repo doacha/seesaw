df38fc54d30a974ad212613f94e5e3a85e3056b7
김봉준 <zune1408@gmail.com>
2023-09-14 15:59:39 +0900

    Chore: #117/미션_개별_컴포넌트-미션_검색창_구현 - conflict 해결이 안되서 로컬에서 일부러 바꿈

--- front/app/types.tsx
+++ front/app/types.tsx
@@ -1,8 +1,46 @@
-export type User = {
-  id: number
-  name: string
-  email: string
+interface User {
+  userEmail?: string
+  userPassword?: string
+  userName?: string
+  userNickname: string
+  userBirth?: string
+  userGender?: boolean
+  userPhoneNumber?: string
+  userImgUrl: string
+  successCnt: number
+  failCnt: number
+  savedMoney: number
 }
+
+interface Mission {
+  mission?: string
+  dueDate?: string
+  missionId?: string
+  missionTitle: string
+  missionMemberCount?: number
+  missionMaxCount?: number
+  missionImgUrl: string
+  missionPurpose?: string
+  missionMinDeposit?: number
+  missionIsPublic?: boolean
+  missionLimit?: number
+  missionPeriod: number
+  missionCycle: number
+  missionStartDate: string
+  missionEndDate: string
+  missionResult: string
+  missionCreationTime?: string
+  missionHostEmail?: string
+  categoryId?: number
+}
+
+interface ImageFile {
+  id: string
+  file?: File
+  url: string
+}
+
+export type { User, Mission, ImageFile }
 
 export interface MissionCardProps {
   missionId: string
