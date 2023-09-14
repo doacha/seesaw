a28e6afb1e6bbb1a18c188b08968baac57661fc1
김봉준 <zune1408@gmail.com>
2023-09-14 15:49:25 +0900

    :sparkles: Feat: #117/미션 개별 컴포넌트-미션 검색창 구현 * 검색창 구현 - 공통컴포넌트 수정 - Capsule   - ToggleCapsule(토글기능 캡슐)과 Capsule(토글X 캡슐)로 분화 - 공통컴포넌트 수정 - Input   - 높이 마진 수정 - types 추가 : 검색용 타입 SearchState 추가 - constant 추가 : cycle 및 period 드롭다운용 array

--- front/app/mission/components/SearchContainer.tsx
+++ front/app/mission/components/SearchContainer.tsx
@@ -1,21 +1,63 @@
 'use client'
 import Capsule from '@/app/components/Capsule'
 import SearchBar from './SearchBar'
-import { categoryList, missionPeriodArray } from '@/app/lib/constants'
+import {
+  categoryList,
+  missionPeriodArray,
+  missionCycleArray,
+} from '@/app/lib/constants'
 import { useRef, useState } from 'react'
+import Button from '@/app/components/Button'
 import styles from './SearchContainer.module.css'
-const SearchContainer = () => {
-  const [periodModalOn, setPeriodModalOn] = useState(false)
+import { faAnglesUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ToggleCapsule from '@/app/components/ToggleCapsule'
+import DropdownCapsule from './DropdownCapsule'
+import type { SearchState } from '@/app/types'
+const SearchContainer = ({
+  state,
+  setState,
+  onClick,
+}: {
+  state: SearchState
+  setState: any
+  onClick: any
+}) => {
+  const [periodDropDownOn, setPeriodDropDownOn] = useState(false)
+  const [cycleDropDownOn, setCycleDropDownOn] = useState(false)
+  // console.log('test etet', state['category'].includes(0), state)
   const periodRef = useRef(null)
-  const handleClick = () => {
-    setPeriodModalOn(true)
+  const handleOpenPeriod = () => {
+    setPeriodDropDownOn(true)
   }
-  const handleModalOff = () => {
-    setPeriodModalOn(false)
+  const handleOpenCycle = () => {
+    setCycleDropDownOn(true)
+  }
+  const handleDropDownOff = () => {
+    setPeriodDropDownOn(false)
+    setCycleDropDownOn(false)
   }
   const handlePeriodClick = (e: React.MouseEvent) => {
-    console.log(e)
     e.stopPropagation()
+  }
+  const handleCapsuleClick = (
+    id: number,
+    isSelected: boolean,
+    type?: string,
+  ) => {
+    if (type == undefined) type = 'category'
+    const newSelected = { ...state }
+    if (isSelected) {
+      // 비활성화시킬 때
+      const idx = newSelected[type].indexOf(id)
+      newSelected[type].splice(idx, 1)
+      setState(newSelected)
+    } else {
+      // 활성화시킬 때
+      newSelected[type].push(id)
+      setState(newSelected)
+    }
+    console.log('aft', newSelected)
   }
   return (
     <div className="rounded-lg bg-background px-5 py-2.5 w-full">
@@ -24,64 +66,112 @@ const SearchContainer = () => {
       <div className={`mt-2.5 overflow-scroll ${styles.delScroll}`}>
         <div className="carousel">
           {categoryList.map((element, idx) => (
-            <Capsule
-              className="carousel-item mr-[15px] h-[14px]"
+            <ToggleCapsule
+              className="carousel-item mr-[15px] h-[14px] py-0"
               bgColor="background-fill"
               textColor={`${idx}`}
               key={idx}
-              changeable={true}
+              value={idx}
+              type="category"
+              select={state['category'].includes(idx)}
+              onClick={handleCapsuleClick}
             >
               {element}
-            </Capsule>
+            </ToggleCapsule>
           ))}
         </div>
       </div>
       <div className="mt-5">주기 및 기간</div>
-      <div className="mt-2.5">
-        <span onClick={handleClick}>
-          <Capsule
-            bgColor="background"
-            textColor="black"
-            isHasBorder={true}
-            className="mr-2.5"
-            onClick={handleClick}
-          >
-            미션 빈도
-          </Capsule>
+      <div className="mt-2.5 relative">
+        <DropdownCapsule
+          className="mr-2.5"
+          onClick={handleOpenPeriod}
+          state={state.period}
+          title="인증 주기"
+          type={1}
+        />
+
+        <DropdownCapsule
+          onClick={handleOpenCycle}
+          state={state.cycle}
+          title="미션 기간"
+          type={2}
+        />
+        <span onClick={onClick}>
+          <FontAwesomeIcon
+            icon={faAnglesUp}
+            className="absolute top-[5px] right-0"
+          />
         </span>
-        <Capsule bgColor="background" textColor="black" isHasBorder={true}>
-          미션 횟수
-        </Capsule>
       </div>
-      {periodModalOn && (
+      {periodDropDownOn && (
         <div
-          className="w-full h-screen fixed bg-black/30 left-0 z-10 bottom-16"
-          onClick={handleModalOff}
+          className="w-full h-screen fixed bg-black/30 left-0 z-10 bottom-16 open:transition-transform"
+          onClick={handleDropDownOff}
         >
           <div
             className="absolute bottom-0 w-full bg-background  rounded-t-lg p-5"
             ref={periodRef}
             onClick={handlePeriodClick}
           >
-            <div className="mb-5 w-full">미션 빈도</div>
+            <div className="mb-5 w-full">인증 주기</div>
             <div className="w-full">
-              {missionPeriodArray.map((element, idx) => {
-                if (idx === 0) return
-                return (
-                  <Capsule
-                    bgColor="background"
-                    textColor="black"
-                    isHasBorder={true}
-                    className="m-[7.5px]"
-                    changeable={true}
-                    onClick={() => {
-                      console.log(idx)
-                    }}
-                  >
-                    {element}
-                  </Capsule>
-                )
-              })}
+              {missionPeriodArray.map((element, idx) => (
+                <ToggleCapsule
+                  bgColor="background-fill"
+                  textColor="black"
+                  className="mr-[15px] mb-[15px]"
+                  key={idx}
+                  value={idx}
+                  type="period"
+                  select={state['period'].includes(idx)}
+                  onClick={handleCapsuleClick}
+                >
+                  {element}
+                </ToggleCapsule>
+              ))}
+              <Button
+                color="primary"
+                label="확인"
+                onClick={handleDropDownOff}
+                size="xs"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      {cycleDropDownOn && (
+        <div
+          className="w-full h-screen fixed bg-black/30 left-0 z-10 bottom-16 open:transition-transform"
+          onClick={handleDropDownOff}
+        >
+          <div
+            className="absolute bottom-0 w-full bg-background  rounded-t-lg p-5"
+            ref={periodRef}
+            onClick={handlePeriodClick}
+          >
+            <div className="mb-5 w-full">미션 기간</div>
+            <div className="w-full">
+              {missionCycleArray.map((element, idx) => (
+                <ToggleCapsule
+                  bgColor="background-fill"
+                  textColor="black"
+                  className="mr-[15px] mb-[15px]"
+                  key={idx}
+                  value={idx}
+                  type="cycle"
+                  select={state['cycle'].includes(idx)}
+                  onClick={handleCapsuleClick}
+                >
+                  {element}
+                </ToggleCapsule>
+              ))}
+              <Button
+                color="primary"
+                label="확인"
+                onClick={handleDropDownOff}
+                size="xs"
+              />
             </div>
           </div>
         </div>
--- front/app/mission/page.tsx
+++ front/app/mission/page.tsx
@@ -1,11 +1,45 @@
+'use client'
+
 import MissionCard from './components/MissionCard'
+import { useState } from 'react'
 import SearchBar from './components/SearchBar'
 import SearchContainer from './components/SearchContainer'
+import SearchContainerSimple from './components/SearchContainerSimple'
 import { dummyMissionCard } from './components/dummy'
+import type { SearchState } from '@/app/types'
+
 const page = () => {
+  const [isSearchContainerFull, setIsSearchContainerFull] = useState(false)
+  const [selectedPeriod, setSelectedPeriod] = useState([])
+  const [selectedSearchState, setSelectedSearchState] = useState<SearchState>({
+    period: [],
+    cycle: [],
+    category: [],
+  })
+  console.log('selected 현황', selectedSearchState)
+  const handleContainerExpand = () => {
+    setIsSearchContainerFull(true)
+  }
+  const handleContainerShrink = () => {
+    setIsSearchContainerFull(false)
+  }
   return (
     <div className="bg-background-fill">
-      <SearchContainer />
+      {!isSearchContainerFull && (
+        <SearchContainerSimple
+          state={selectedSearchState}
+          onClick={handleContainerExpand}
+        />
+        // selected만 보내면 됨
+      )}
+      {isSearchContainerFull && (
+        <SearchContainer
+          state={selectedSearchState}
+          setState={setSelectedSearchState}
+          onClick={handleContainerShrink}
+        />
+        //set이랑 state 다 보내야함
+      )}
       <div className="flex flex-wrap gap-5 mx-5">
         <MissionCard data={dummyMissionCard} />
         <MissionCard data={dummyMissionCard} />
