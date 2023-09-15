908a2a730e5c165f1acecfe627c45b2508b57452
Hanna Kim <gksskrn0158@gmail.com>
2023-09-15 18:12:14 +0900

    :sparkles: Feat

--- front/app/components/FastMakeButton.tsx
+++ front/app/components/FastMakeButton.tsx
@@ -2,12 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const FaskMakeButton = ({ onClick }: { onClick: any }) => {
+  // fast 버튼은 가계부에만 넣는건가?
   return (
-    <div className="fixed left-[295px] z-1 top-[450px] rounded-full bg-background-fill w-[60px] h-[60px] m-0 relative flex justify-center items-center drop-shadow-md">
+    <div
+      className="z-50 flex justify-center items-center rounded-full bg-primary-container w-[70px] h-[70px] m-0 drop-shadow-md"
+      onClick={onClick}
+    >
       <FontAwesomeIcon
         icon={faPlus}
         size="2xl"
-        className="text-black absolute m-auto"
+        className=" text-white absolute m-auto"
       />
     </div>
   )
