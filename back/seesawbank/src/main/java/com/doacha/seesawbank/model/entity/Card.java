3be474adbd33962c5b18ddce0f749828ba9616ef
박시원 <tldnjs324@naver.com>
2023-09-15 15:53:22 +0900

    :sparkles: Feat: Add file Structure

---
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/model/entity/Card.java
@@ -0,0 +1,36 @@
+package com.doacha.seesawbank.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Card {
+
+    @Id
+    @Column(name = "card_num")
+    private String cardNum;
+
+    @ManyToOne
+    @JoinColumn(name="account_num",nullable = false)
+    private Account accoount;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable = false)
+    private User user;
+
+    @Column(name = "card_payment_date")
+    private Timestamp cardPaymentDate;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+}
