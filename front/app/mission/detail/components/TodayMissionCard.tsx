c8fbad2e17840209dccaa8500459954c59e5837d
hehezune <funcodezune@gmail.com>
2023-09-16 20:07:20 +0900

    :sparkles: Feat: #120/미션 개별 컴포넌트-오늘의 미션 현황 카드 구현 - 금액 부분 양끝으로 정렬

--- front/app/mission/detail/components/TodayMissionCard.tsx
+++ front/app/mission/detail/components/TodayMissionCard.tsx
@@ -17,7 +17,6 @@ const TodayMissionCard = ({
   targetPrice: number
   onClick: any
 }) => {
-  console.log(targetPrice, data.spending)
   return (
     <div className="carousel-item flex flex-col rounded-[25px] bg-background w-[124px] h-[229px] mr-5 drop-shadow-md">
       {/* 카드 상단 - 프로필 */}
@@ -42,13 +41,13 @@ const TodayMissionCard = ({
       </div>
       {/* 카드 하단 - 지출내역 */}
       <div className="m-auto">
-        <div className="px-[20px] mb-2.5">
+        <div className="px-[20px] mb-2.5 flex justify-between">
           <span className="text-[10px] mr-2">지출</span>
           <span className="font-scDreamExBold text-error">
             {data.spending.toLocaleString('ko-KR')}
           </span>
         </div>
-        <div className="px-[20px]">
+        <div className="px-[20px] flex justify-between">
           <span className="text-[10px] mr-2">잔액</span>
           <span className="font-scDreamExBold text-primary">
             {data.balance.toLocaleString('ko-KR')}
