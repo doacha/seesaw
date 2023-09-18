39ea4070ba0d9fa5ba31bcede62e19edf76aa209
Hanna Kim <gksskrn0158@gmail.com>
2023-09-18 17:54:21 +0900

    :sparkles: Feat : #237 소비그래프 막대 그래프 레이아웃

---
+++ front/app/report/components/TextCard.tsx
@@ -0,0 +1,14 @@
+const TextCard = () => {
+  return (
+    <div className="flex flex-row h-20 w-full bg-white rounded-lg">
+      <div className=" my-auto mx-5 w-full">
+        <p className=" font-scDreamLight text-base">
+          저번달에 비해 얼마 덜썼어요
+        </p>
+        <p className=" font-envR text-lg">당신은 절약왕!</p>
+      </div>
+    </div>
+  )
+}
+
+export default TextCard
