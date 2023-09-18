fc704fffb01e0ec0a61cf73c107f0b8a299839f7
박시원 <tldnjs324@naver.com>
2023-09-18 13:00:14 +0900

    :sparkles: Feat: login, signup

---
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/model/dto/member/MemberResponse.java
@@ -0,0 +1,22 @@
+package com.doacha.seesawbank.model.dto.member;
+
+import com.doacha.seesawbank.model.entity.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberResponse {
+    private final String memberId;
+
+    private final String memberName;
+
+    private MemberResponse(String memberId, String memberName) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public static MemberResponse of(Member member) {
+        return new MemberResponse(
+                member.getMemberId(),
+                member.getMemberName());
+    }
+}
