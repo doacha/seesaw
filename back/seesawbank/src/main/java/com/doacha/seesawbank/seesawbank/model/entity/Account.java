22e6dffd8a6032bd620e33d8fb21b11456160baa
박시원 <tldnjs324@naver.com>
2023-09-18 17:51:08 +0900

    :sparkles: Feat: Account service

--- back/seesawbank/src/main/java/com/doacha/seesawbank/seesawbank/model/entity/Account.java
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/seesawbank/model/entity/Account.java
@@ -14,10 +14,17 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 100001,
+        allocationSize = 1)
 public class Account {
     @Id
-    @Column(name="account_num",nullable=false)
-    private String accountNum;
+    @Column(name="account_num")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    private int accountNum;
 
     @ManyToOne
     @JoinColumn(name="member_id",referencedColumnName = "member_id",nullable=false)
@@ -27,10 +34,11 @@ public class Account {
     private String accountName;
 
     @Column(name="account_type",nullable=false)
+    @ColumnDefault("1") // 만들 때는 적금 계좌만 만들어서
     private int accountType;
 
     @Column(name="aacount_interest_rate",nullable=false)
-    private int accountInterestRate;
+    private float accountInterestRate;
 
     @Column(name="account_is_inactivate",nullable=false)
     @ColumnDefault("0")
@@ -41,7 +49,5 @@ public class Account {
 
     @Column(name="account_password",nullable=false)
     private String accountPassword;
-
-
 
 }
