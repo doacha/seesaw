1cd728b4cf1128265cd5e79336a87eb6a75618e7
LeeKyeongho <rudgh1366@naver.com>
2023-09-18 12:33:16 +0900

    :sparkles: Feat: seesaw SpendingServiceImpl 카테고리 관련 수정

--- back/seesawbank/src/main/java/com/doacha/seesawbank/model/entity/AccountTransaction.java
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/model/entity/AccountTransaction.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Comment;
-
-import java.sql.Time;
 import java.sql.Timestamp;
 
 @Entity
