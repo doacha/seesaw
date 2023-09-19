df14fcbc510d59197723fdfa28f2886abd5e7fa4
김봉준 <zune1408@gmail.com>
2023-09-19 18:27:42 +0900

    :sparkles: Feat: FE/128/미션_개별_컴포넌트-나의_현황_회차별_미션_기록_구현 - 음수(미션 실패 상태)를 표현한 차트로 개선중 - 2 * h 의 길이를 가지는 div 내에 음수, 양수냐에 따라 absolute position, h 높이의 div를 구현하는 방식

--- front/app/(mission)/mission/detail/components/MissionGraphBar.tsx
+++ front/app/(mission)/mission/detail/components/MissionGraphBar.tsx
@@ -1,21 +1,37 @@
 interface Props {
   round: number
-  length: string
+  length: number
   amount: number
+  isToday?: boolean
   bgColor: string
   txtColor: string
 }
 
+// 잔액 절댓갑셍 대한 length를 받는다
+// 최상위 div의 h를 2 * length로 잡고 relative화 해준다
+// 하위 div로 막대기 (absolute, top-0 or bottom-0, color 세팅) 잡아준다.
+// 금액 또한 absolute로 최상위 div의 하위 div로서 잡아준다.
+
+// 첫번째 컴포넌트도 확인할 수 있는 ~
+
 const MissionGraphBar = (props: Props) => {
-  console.log(props.length)
+  const [bgColor, position, textPosition] =
+    props.amount >= 0
+      ? ['bg-primary-container', 'top-0', 'top-[-10px]']
+      : ['bg-seesaw-red-100', 'bottom-0', 'bottom-[-10px]']
+  console.log(props.length, '기리')
+
   return (
-    <div className="flex flex-col items-center gap-2">
-      <div className={`${props.txtColor} text-[10px]`}>
+    <div
+      className="flex flex-col items-center gap-2 bg-background-fill relative"
+      style={{ height: `${props.length * 2}px` }}
+    >
+      <div className={`text-black text-[10px] absoulte ${textPosition}`}>
         {props.amount.toLocaleString()}
       </div>
       <div
-        className={`${props.bgColor} w-[30px] rounded-md`}
-        style={{ height: props.length }}
+        className={`${bgColor} w-[30px] rounded-md absolute bottom-0 ${position}`}
+        style={{ height: `${props.length}px` }}
       ></div>
       <div className="text-xs w-fit whitespace-nowrap">{props.round}회차</div>
     </div>
