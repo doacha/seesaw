2836d427bdb31669a291633d5b283efeee9f02ce
LeeKyeongho <rudgh1366@naver.com>
2023-09-19 13:22:38 +0900

    :sparkles: Feat:월말 소비 분석 API 완성, 카테고리 분류 제작중

--- back/seesawbank/src/main/java/com/doacha/seesawbank/model/service/CardTransactionService.java
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/model/service/CardTransactionService.java
@@ -1,7 +1,7 @@
 package com.doacha.seesawbank.model.service;
 
 
-import com.doacha.seesawbank.model.dto.SpendingResponse;
+import com.doacha.seesawbank.model.dto.cardTransaction.SpendingResponse;
 import com.doacha.seesawbank.model.entity.Member;
 import com.doacha.seesawbank.repository.CardTransactionRepository;
 import com.doacha.seesawbank.repository.MemberRepository;
