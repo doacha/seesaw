32511060f689cab43b4e47c91c22dc845219119f
Hanna Kim <gksskrn0158@gmail.com>
2023-09-19 16:56:42 +0900

    :sparkles: Feat : #261  소비리포트 막대그래프 - 주별 막대그래프 처리 - 각 탭마다 월, 주, 일별 막대그래프 연결 처리 해야함 - 그래프 text 작성 필요

--- front/app/report/components/ReportVerticalGraphBar.tsx
+++ front/app/report/components/ReportVerticalGraphBar.tsx
@@ -4,21 +4,79 @@ interface Props {
   amount?: number
   bgColor: string
   txtColor: string
+  amountListLength: number
+  activeCalendarTab: string
 }
+
 const ReportVerticalGraphBar = (props: Props) => {
-  return (
-    <div className="flex flex-col items-center">
-      {/* <div className={`${props.txtColor} text-xs`}>
-        {props.amount.toLocaleString()}
-      </div> */}
-      {/* 월이면 w-10px, 일이면 5px, 주면 w-15 */}
-      <div
-        className={`${props.bgColor} w-[20px] rounded-md mx-2`}
-        style={{ height: props.length }}
-      ></div>
-      {/* <div className="text-xs">{props.round + 1}일</div> */}
-    </div>
-  )
+  const ladel: string[] = ['월', '주차', '일']
+
+  const getTabContent = () => {
+    switch (props.activeCalendarTab) {
+      case 'tab1':
+        return (
+          <div className="flex flex-col items-center">
+            {/* 월이면 w-10px, 일이면 5px, 주면 w-15 */}
+            {props.amount && props.round === props.amountListLength - 1 && (
+              <div
+                className="tooltip tooltip-open"
+                data-tip={`${props.amount.toLocaleString('ko-KR')}`}
+              ></div>
+            )}
+            <div
+              className={`${props.bgColor} w-[20px] rounded-md mx-2`}
+              style={{ height: props.length }}
+            ></div>
+            <div className="text-xs">
+              {props.round + 1}
+              {ladel[0]}
+            </div>
+          </div>
+        )
+      case 'tab2':
+        return (
+          <div className="flex flex-col items-center">
+            {props.amount && props.round === props.amountListLength - 1 && (
+              <div
+                className="tooltip tooltip-open"
+                data-tip={`${props.amount.toLocaleString('ko-KR')}`}
+              ></div>
+            )}
+            <div
+              className={`${props.bgColor} w-[30px] rounded-md mx-2`}
+              style={{ height: props.length }}
+            ></div>
+            <div className="text-xs">
+              {props.round + 1}
+              {ladel[1]}
+            </div>
+          </div>
+        )
+      case 'tab3':
+        return (
+          <div className="flex flex-col items-center">
+            {props.amount && props.round === props.amountListLength - 1 && (
+              <div
+                className="tooltip tooltip-open"
+                data-tip={`${props.amount.toLocaleString('ko-KR')}`}
+              ></div>
+            )}
+            <div
+              className={`${props.bgColor} w-[20px] rounded-md mx-2`}
+              style={{ height: props.length }}
+            ></div>
+            <div className="text-xs">
+              {props.round + 1}
+              {ladel[2]}
+            </div>
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  return getTabContent()
 }
 
 export default ReportVerticalGraphBar
--- front/app/report/components/SumGraphCard.tsx
+++ front/app/report/components/SumGraphCard.tsx
@@ -4,19 +4,21 @@ import { useState, useEffect } from 'react'
 import ReportVerticalGraphBar from './ReportVerticalGraphBar'
 import ReportTab from './ReportTab'
 import { Spending } from '@/app/types'
+import MonthSumGraphCard from './MonthSumGraphCard'
+
 interface SumGraphCardProps {
   handleCalendarTabChange: (tab: string) => void
   activeCalendarTab: string
-  monthSumList: Spending[]
+  sumList: Spending[]
+  groupedSpending: Record<string, number>
 }
 const SumGraphCard = ({
   handleCalendarTabChange,
   activeCalendarTab,
-  monthSumList,
+  sumList,
+  groupedSpending,
 }: SumGraphCardProps) => {
-  console.log(monthSumList.length)
-  // undefined는 어떻게 처리하는게 좋을까
-  const amountList: number[] = monthSumList.map(
+  let amountList: number[] = sumList.map(
     (element) => element.spendingCostSum as number,
   )
   const averageAmount = () => {
@@ -31,7 +33,7 @@ const SumGraphCard = ({
   const [meanLenght, setMeanLength] = useState<string>('')
 
   useEffect(() => {
-    // 계산에 필요한 변수 초기화
+    // 평균 지출 금액을 파악하기 위한 빨간 줄.. 슬프게도 미완..
     let mean = 0
     const tmpLengthList: string[] = []
 
@@ -41,14 +43,13 @@ const SumGraphCard = ({
       tmpLengthList.push(tmp)
     }
 
-    // 상태 업데이트는 한 번만 수행
     setLengthList(tmpLengthList)
     setMeanLength('h-[' + Math.round(mean / amountList.length) + 'px]')
 
-    // 디버깅용 콘솔 출력
     console.log(tmpLengthList)
     console.log(mean)
-  }, []) // amountList가 변경될 때마다 실행
+  }, [])
+
   return (
     // css가 아주 그냥 꼬였어
     <div className="h-fit flex w-full bg-background rounded-lg">
@@ -57,42 +58,54 @@ const SumGraphCard = ({
           handleCalendarTabChange={handleCalendarTabChange}
           activeCalendarTab={activeCalendarTab}
         />
-        <div className="w-full flex relative">
-          {/* <div className="flex z-1 bg-blue-100 m-5 p-5 rounded-lg overflow-auto "> */}
-          <div className="flex w-full bg-blue-100 m-5 p-5 rounded-lg overflow-auto ">
-            <div className="flex z-1">
-              <div className="flex items-end justify-between">
+        {activeCalendarTab == 'tab1' ? (
+          <>
+            <div className="w-full flex">
+              <div className="flex w-full justify-between bg-blue-100 m-5 px-5 pt-10 pb-5 rounded-lg overflow-auto">
                 {amountList.map((amount, index) => (
-                  <ReportVerticalGraphBar
-                    amount={amount}
-                    length={lengthList[index]}
-                    round={index}
-                    bgColor={
-                      index === amountList.length - 1
-                        ? 'bg-primary'
-                        : 'bg-blue-300'
-                    }
-                    txtColor={
-                      index === amountList.length - 1
-                        ? 'text-secondary'
-                        : 'text-black'
-                    }
-                    key={index}
-                    // meanLenght={meanLenght}
-                  />
+                  <>
+                    <div className="flex items-end justify-between">
+                      <ReportVerticalGraphBar
+                        amount={amount}
+                        length={lengthList[index]}
+                        round={index}
+                        bgColor={
+                          index === amountList.length - 1
+                            ? 'bg-primary'
+                            : 'bg-blue-300'
+                        }
+                        txtColor={
+                          index === amountList.length - 1
+                            ? 'text-secondary'
+                            : 'text-black'
+                        }
+                        key={index}
+                        amountListLength={amountList.length}
+                        activeCalendarTab={activeCalendarTab}
+
+                        // meanLenght={meanLenght}
+                      />
+                    </div>
+                  </>
                 ))}
-                <div
-                  className={`w-full absolute border-t-4 border-red-500 ${meanLenght}`}
-                ></div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="mx-5 mb-5 w-full">
-          <div className=" bg-slate-100 rounded-lg w-full h-fit">
-            평균지출금액
-          </div>
-        </div>
+            <div className="mx-5 mb-5 p-2 bg-slate-100 rounded-lg  flex justify-between">
+              <div className="ml-1 my-auto font-scDreamRegular text-xs">
+                평균지출금액
+              </div>
+              <div className="my-auto text-xs font-scDreamExBold">
+                {Math.round(averageAmount()).toLocaleString('ko-KR')}원
+              </div>
+            </div>
+          </>
+        ) : (
+          <MonthSumGraphCard
+            activeCalendarTab="tab2"
+            groupedSpending={groupedSpending}
+            handleCalendarTabChange={handleCalendarTabChange}
+          />
+        )}
       </div>
     </div>
   )
