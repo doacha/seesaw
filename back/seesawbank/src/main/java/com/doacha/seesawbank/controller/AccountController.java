6a0fc5d190b864555f7d96414582d67474a40482
박시원 <tldnjs324@naver.com>
2023-09-20 10:03:44 +0900

    :sparkles: Feat: check balance api

--- back/seesawbank/src/main/java/com/doacha/seesawbank/controller/AccountController.java
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.doacha.seesawbank.controller;
 import com.doacha.seesawbank.model.dto.account.AccountResponse;
 import com.doacha.seesawbank.model.dto.account.CreateAccountRequest;
 import com.doacha.seesawbank.model.service.AccountService;
+import com.doacha.seesawbank.model.service.AccountTransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final AccountTransactionService accountTransactionService;
 
     @PostMapping("/create")
     public AccountResponse createAccount(@RequestBody CreateAccountRequest createAccountRequest) {
         return accountService.createAccount(createAccountRequest);
+    }
+
+    @PostMapping("/checkRecentBalance")
+    public int checkRecentBalance(@RequestBody String accountNum){
+        return accountTransactionService.checkRecentBalance(accountNum);
     }
 }
