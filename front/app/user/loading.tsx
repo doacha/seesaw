a39ebf10ba0aece4ebb96e5a22221da63871d4cd
jay <jjwoong1733@gmail.com>
2023-09-22 16:38:06 +0900

    :sparkles: Feat: #196 내 계좌 페이지 조립 - navbar 비활성화 조건 추가 - 소비예측페이지 추가 - 시소뱅크 페이지 추가 (1원인증용) - 완료미션 그룹통계 컨텐츠 추가 - 적금등록 페이지 추가

---
+++ front/app/user/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="w-full h-[600px] flex items-center justify-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  )
+}
+
+export default loading
