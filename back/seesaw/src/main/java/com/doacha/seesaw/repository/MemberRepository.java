37576e658fe1663a461122f1900e3dcb125d2978
박시원 <tldnjs324@naver.com>
2023-09-22 12:54:14 +0900

    :sparkles: Feat: MyPage API

--- back/seesaw/src/main/java/com/doacha/seesaw/repository/MemberRepository.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package com.doacha.seesaw.repository;
 
 import com.doacha.seesaw.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     boolean existsByMemberEmail(String memberEmail);
 
-    Optional<Member> findByMemberEmail(String memberEmail);
+    Optional<Member> findByMemberEmail(@Param("memberEmail") String memberEmail);
 }
