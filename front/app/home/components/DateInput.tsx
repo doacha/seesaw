e948bd5956c31065915343c4b41db50abb91a995
Hanna Kim <gksskrn0158@gmail.com>
2023-09-26 17:55:48 +0900

    :sparkles: Feat : #283 가계부 목록 및 세부 내용 API 연결

--- front/app/home/components/DateInput.tsx
+++ front/app/home/components/DateInput.tsx
@@ -2,8 +2,9 @@
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 interface Props {
-  value: string
+  value: Date
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 const DateInput = ({ value, onChange }: Props) => {
@@ -11,7 +12,18 @@ const DateInput = ({ value, onChange }: Props) => {
   const clickDate = () => {
     setClickDa(!clickDa)
   }
-
+  const formatDate = (date: Date): string => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+      hour: 'numeric',
+      minute: 'numeric',
+    }
+    const formatter = new Intl.DateTimeFormat('ko-KR', options)
+    return formatter.format(date)
+  }
   return (
     <div className="w-full flex flex-row gap-1 py-4 border-b-2 border-outline-container">
       <div className="w-28">
@@ -19,7 +31,9 @@ const DateInput = ({ value, onChange }: Props) => {
       </div>
       <div className="flex my-auto w-full justify-between">
         {!clickDa ? (
-          <p className="my-auto font-scDreamLight text-xs">{value}</p>
+          <p className="my-auto font-scDreamLight text-xs">
+            {formatDate(value)}
+          </p>
         ) : (
           <input
             className="w-full mr-5 font-scDreamLight text-xs"
