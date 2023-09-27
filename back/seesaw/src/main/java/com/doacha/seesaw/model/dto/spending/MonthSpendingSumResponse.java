dfd078bcafbf2ba4aae2458105193659b1e8f452
LeeKyeongho <rudgh1366@naver.com>
2023-09-27 15:22:12 +0900

    :sparkles: Feat:Spending 월합계 불러오기 연도 추가, 미션 평균과 해당 카테고리 전체 평균 비교 제작

--- back/seesaw/src/main/java/com/doacha/seesaw/model/dto/spending/MonthSpendingSumResponse.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/dto/spending/MonthSpendingSumResponse.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MonthSpendingSumResponse {
     private long spendingCostSum;
+    private int spendingYear;
     private int spendingMonth;
     private String memberEmail;
 
