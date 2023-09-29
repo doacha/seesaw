88938d2435042ea9aac1f0ebb0cecc5d3036f831
hehezune <funcodezune@gmail.com>
2023-09-29 11:06:00 +0900

    :sparkles: Feat: FE/136/미션_개별_컴포넌트-미션_상세_대기자_목록_카드_구현 - 미션이 시작하지 않았을 경우, 미션 상세에서 보여지는 대기자 목록 컴포넌트 구현 - 방장 여부를 crown으로 보여지고, 그 외 다른 특이 기능은 없음

--- front/app/(mission)/mission/[missionId]/components/MissionWaitingList.tsx
+++ front/app/(mission)/mission/[missionId]/components/MissionWaitingList.tsx
@@ -1,5 +1,47 @@
-const MissionWaitingList = () => {
-  return <div></div>
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCrown } from '@fortawesome/free-solid-svg-icons'
+interface MemberList {
+  memberImgUrl: string
+  memberName: string
+  isHost: boolean
+}
+const memberListdummy: MemberList[] = Array(10).fill({
+  memberImgUrl: '/차차_군침이.jpg',
+  memberName: '차차아버님',
+  isHost: false,
+})
+
+memberListdummy[0].isHost = true
+
+const WaitingMemberCard = ({ data }: { data: MemberList }) => {
+  return (
+    <div className="flex flex-row bg-primary-container items-center relative">
+      <Image
+        alt="member profile image"
+        src={data.memberImgUrl}
+        width={25}
+        height={25}
+        className="rounded-full mr-2.5"
+      />
+      <div className="text-sm">{data.memberName}</div>
+      {data.isHost && (
+        <FontAwesomeIcon
+          icon={faCrown}
+          className="text-primary absolute right-0"
+        />
+      )}
+    </div>
+  )
 }
 
+const MissionWaitingList = () => {
+  return (
+    <div className="bg-background m-5 rounded-lg grid grid-cols-2 grid-flow-row p-5 gap-y-2.5 gap-x-5">
+      {memberListdummy.map((element, idx) => (
+        <WaitingMemberCard data={element} key={element.memberName} />
+      ))}
+    </div>
+  )
+}
 export default MissionWaitingList
