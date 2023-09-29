720109f73da600435ce53d365cda52d49f79916f
iamjione <kimjw3928@gmail.com>
2023-09-29 17:23:24 +0900

    :sparkles: Feat: 카드 거래내역 조회 API
    - 사용자 아이디, 조회 시작일시, 조회 종료일시로 카드 거래내역 조회

--- back/seesawbank/src/main/java/com/doacha/seesawbank/model/service/CardTransactionService.java
+++ back/seesawbank/src/main/java/com/doacha/seesawbank/model/service/CardTransactionService.java
@@ -1,29 +1,35 @@
 package com.doacha.seesawbank.model.service;
 
 
-import com.doacha.seesawbank.model.dto.cardTransaction.SpendingResponse;
-import com.doacha.seesawbank.model.entity.Member;
+import com.doacha.seesawbank.model.dto.cardTransaction.GetCardTransactionRequest;
+import com.doacha.seesawbank.model.dto.cardTransaction.GetCardTransactionResponse;
 import com.doacha.seesawbank.repository.CardTransactionRepository;
 import com.doacha.seesawbank.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
-
 public class CardTransactionService {
+
     @Autowired
     CardTransactionRepository cardTransactionRepository;
-    @Autowired
-    MemberRepository memberRepository;
-    public List<SpendingResponse> findAllByMemberId (String memberId){
-        Optional<Member> member = memberRepository.findById(memberId);
-        List<SpendingResponse> cardTranscationList = findAllByMemberId(member.get().getMemberId());
-        return cardTranscationList;
-    }
 
+//    public List<SpendingResponse> findAllByMemberId (String memberId){
+//        Optional<Member> member = memberRepository.findById(memberId);
+//        List<SpendingResponse> cardTranscationList = findAllByMemberId(member.get().getMemberId());
+//        return cardTranscationList;
+//    }
+
+    public List<GetCardTransactionResponse> getCardTransactionList(GetCardTransactionRequest getCardTransactionRequest) {
+        String memberId = getCardTransactionRequest.getMemberId();
+        Timestamp startDateTime = getCardTransactionRequest.getStartDateTime();
+        Timestamp endDateTime = getCardTransactionRequest.getEndDateTime();
+
+        return cardTransactionRepository.findCardTransactionResponseByCardTransactionRequest(memberId, startDateTime, endDateTime);
+    }
 }
