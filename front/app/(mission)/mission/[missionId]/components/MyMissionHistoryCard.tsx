866d67f50712c3368fe1229d61100af00307a617
hehezune <funcodezune@gmail.com>
2023-10-01 10:09:10 +0900

    :sparkles: Feat: FE/284/게시글_상세_페이지_API_연결 - 게시글 상세 API 연결 - 상세 거래 내역은 미션 상세에서 store를 통해 가져오므로, url 직접 접근했을 때 거래내역이 뜨지 않음 - 고로 URL을 통한 직접 접근을 막아야함 (접근 후 자동으로 router.push해보려 했으나 렌더링 중엔 다른 곳으로 넘기면 안된다는 오류를 해결하지 못함)

--- front/app/(mission)/mission/[missionId]/components/MyMissionHistoryCard.tsx
+++ front/app/(mission)/mission/[missionId]/components/MyMissionHistoryCard.tsx
@@ -1,5 +1,6 @@
 import { getCycleTerm } from '../../../util'
 import { GroupStatusProps } from '@/app/types'
+import Link from 'next/link'
 enum Property {
   'recordNumber',
   'recordStartDate',
@@ -31,39 +32,41 @@ const MyMissionHistoryCard = ({
     sumOfSpending += data[i][1]
   }
   return (
-    <div className="my-2.5 mt-5 p-2 shadow-md rounded-lg">
-      {/* 제목 */}
-      <div className="mb-[10px]">
-        <span
-          className={`${
-            data[Property.recordStatus] !== 2 ? 'text-primary' : 'text-error'
-          } font-scDreamExBold mr-[10px]`}
-        >
-          {data[Property.recordStatus] !== 2 ? '성공' : '실패'}
-        </span>
-        <span className="font-scDreamExBold mr-[10px]">
-          {data[Property.recordNumber]}회차
-        </span>
-        <span className="text-[10px] text-outline">
-          {getCycleTerm(
-            propsData.missionStartDate,
-            data[Property.recordNumber],
-            propsData.missionPeriod,
-          )}
-        </span>
-      </div>
-      {/* 상세목록 */}
-      {recordList.map((element, idx) => (
-        <div className="md-[5px] flex justify-between" key={idx}>
-          <span>{element[0]}</span>
-          <span>{element[1].toLocaleString('ko-KR')}</span>
+    <Link href={`${propsData.missionId}/${data[Property.recordNumber]}`}>
+      <div className="my-2.5 mt-5 p-2 shadow-md rounded-lg">
+        {/* 제목 */}
+        <div className="mb-[10px]">
+          <span
+            className={`${
+              data[Property.recordStatus] !== 2 ? 'text-primary' : 'text-error'
+            } font-scDreamExBold mr-[10px]`}
+          >
+            {data[Property.recordStatus] !== 2 ? '성공' : '실패'}
+          </span>
+          <span className="font-scDreamExBold mr-[10px]">
+            {data[Property.recordNumber]}회차
+          </span>
+          <span className="text-[10px] text-outline">
+            {getCycleTerm(
+              propsData.missionStartDate,
+              data[Property.recordNumber],
+              propsData.missionPeriod,
+            )}
+          </span>
         </div>
-      ))}
-      <hr className="my-[10px]" />
-      <div className="text-right font-scDreamExBold">
-        {sumOfSpending.toLocaleString('ko-KR')}
+        {/* 상세목록 */}
+        {recordList.map((element, idx) => (
+          <div className="md-[5px] flex justify-between" key={idx}>
+            <span>{element[0]}</span>
+            <span>{element[1].toLocaleString('ko-KR')}</span>
+          </div>
+        ))}
+        <hr className="my-[10px]" />
+        <div className="text-right font-scDreamExBold">
+          {sumOfSpending.toLocaleString('ko-KR')}
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
