4dde23d5777d3ad1a0100766384bdd35b7d94731
iamjione <kimjw3928@gmail.com>
2023-10-02 15:43:54 +0900

    :sparkles: Feat: 나의 미션 목록 API & 미션 목록에 카테고리 아이디 추가

--- back/seesaw/src/main/java/com/doacha/seesaw/model/service/MissionService.java
+++ back/seesaw/src/main/java/com/doacha/seesaw/model/service/MissionService.java
@@ -3,6 +3,7 @@ package com.doacha.seesaw.model.service;
 import com.doacha.seesaw.exception.NoContentException;
 import com.doacha.seesaw.model.dto.mission.*;
 import com.doacha.seesaw.model.entity.Mission;
+import com.doacha.seesaw.repository.MemberMissionRepository;
 import com.doacha.seesaw.repository.MissionRepository;
 import com.doacha.seesaw.repository.RecordRepository;
 import com.doacha.seesaw.repository.SpendingRepository;
@@ -30,6 +31,9 @@ public class MissionService {
 
     @Autowired
     MissionRepository missionRepository;
+
+    @Autowired
+    MemberMissionRepository memberMissionRepository;
 
     @Autowired
     RecordRepository recordRepository;
@@ -238,5 +242,8 @@ public class MissionService {
         }
     }
 
-
+    // 나의 미션 불러오기
+    public List<MissionListResponse> getMyMissionList(Pageable pageable, String memberEmail) {
+        return missionRepository.findMissionListResponseByMemberEmail(memberEmail, pageable);
+    }
 }
