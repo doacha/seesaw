a4eae40bb5311686480a6308ef7feede69edcb60
hehezune <funcodezune@gmail.com>
2023-10-03 17:31:03 +0900

    :sparkles: Feat: FE/MissionCreateInvalidCheck - 입력 유효값 검사

--- front/app/(mission)/mission/create/components/ImageInput.tsx
+++ front/app/(mission)/mission/create/components/ImageInput.tsx
@@ -4,6 +4,7 @@ import { faCamera } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import { ImageFile } from '@/app/types'
+import Image from 'next/image'
 const ImageInput = ({
   state,
   setState,
@@ -28,19 +29,21 @@ const ImageInput = ({
       <div className="font-scDreamExBold mb-5">
         그룹 사진을 설정해보시겠어요?
       </div>
-      <div className="flex items-center justify-center w-full">
+      <div className="flex items-center justify-center w-full relative">
         <label
           htmlFor="file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+          className="flex flex-col items-center justify-center w-full h-[205px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
         >
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <FontAwesomeIcon icon={faCamera} size="5x" color="#787D85" />
-            <p className="my-3 text-sm text-outline">
-              <span className="font-scDreamExBold">
-                최대 한장 올릴 수 있습니다
-              </span>
-            </p>
-          </div>
+          {state.imgFile.url === '' && (
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <FontAwesomeIcon icon={faCamera} size="5x" color="#787D85" />
+              <p className="my-3 text-sm text-outline">
+                <span className="font-scDreamExBold">
+                  최대 한장 올릴 수 있습니다
+                </span>
+              </p>
+            </div>
+          )}
           <input
             type="file"
             accept="image/*"
@@ -48,13 +51,22 @@ const ImageInput = ({
             id="file"
             onChange={handleImageChange}
           />
+          {state.imgFile.url && (
+            <div className="absolute left-[50%] translate-x-[-50%] ">
+              <Image
+                src={state.imgFile.url}
+                width={10}
+                height={10}
+                alt="그룹 이미지"
+                style={{
+                  width: '310px',
+                  height: '205px',
+                  objectFit: 'cover',
+                }}
+              />
+            </div>
+          )}
         </label>
-        {/* 이미지 미리보기
-      {image && (
-        <div className="mt-4">
-          <img src={image} alt="Uploaded" width="200" />
-        </div>
-      )} */}
       </div>
     </div>
   )
