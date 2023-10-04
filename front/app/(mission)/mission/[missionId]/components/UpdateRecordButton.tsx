3d2e4f8753e650e412a1657960dc9f183f4334aa
hehezune <funcodezune@gmail.com>
2023-10-05 07:16:29 +0900

    :bug: Fix: FE/updateRecord - 리코드 수정 접근했을때 발생하는 에러 해결중

--- front/app/(mission)/mission/[missionId]/components/UpdateRecordButton.tsx
+++ front/app/(mission)/mission/[missionId]/components/UpdateRecordButton.tsx
@@ -3,7 +3,7 @@ import FaskMakeButton from '@/app/components/FastMakeButton'
 import { recordListStore } from '@/stores/recordListStore'
 const UpdateRecordButton = () => {
   const { recordStatus } = recordListStore()
-
+  console.log('레코드버ㅡㄴ', recordStatus)
   return (
     <FaskMakeButton
       path={`${recordStatus.missionId}/${recordStatus.todayRecordId}/create`}
