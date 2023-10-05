53bc2970b9a5cf409e5a633b1cd434742e31089b
김봉준 <zune1408@gmail.com>
2023-10-05 12:50:17 +0900

    :sparkles: Feat: FE/updateRecord - 게시글 수정 잘 안되던것 해결 - 프사 문제 등 각종 css 문제 해결

--- front/app/(mission)/mission/[missionId]/[recordId]/components/RecordContentContainer.tsx
+++ front/app/(mission)/mission/[missionId]/[recordId]/components/RecordContentContainer.tsx
@@ -15,6 +15,7 @@ import { RecordList } from '@/app/types'
 import { useRef, useState, useEffect } from 'react'
 import { memberEmailStore } from '@/stores/memberEmail'
 import { useRouter } from 'next/navigation'
+import { useQuery } from '@tanstack/react-query'
 interface RecordDetailProps {
   recordId: number
   recordContent: string
@@ -27,63 +28,89 @@ interface RecordDetailProps {
   memberImgUrl: string
 }
 const RecordContentContainer = ({
-  propsData,
+  spendingHistory,
+  recordId,
 }: {
-  propsData: RecordDetailProps
+  spendingHistory: Array<{
+    spendingTitle: string
+    spendingCost: number
+  }>
+  recordId: number
 }) => {
-  console.log('프롭스확인', propsData)
-  const { memberEmail } = memberEmailStore()
+  const { data, isSuccess } = useQuery({
+    queryKey: ['recordDetail', recordId],
+    queryFn: () => getRecordContent(recordId),
+    staleTime: 1000,
+  })
+  const { memberEmail, memberNickname } = memberEmailStore()
   const { recordList, recordStatus } = recordListStore()
   const [isOpened, setIsOpened] = useState<Boolean>(false)
   const checkRef = useRef<HTMLInputElement>(null)
   const router = useRouter()
+  const [recordDetail, setRecordDetail] = useState<RecordDetailProps>({
+    recordId: 0,
+    recordContent: '',
+    recordWriteTime: '',
+    recordTotalCost: 0,
+    recordNumber: 0,
+    recordStatus: 0,
+    memberEmail: memberEmail,
+    memberNickname: memberNickname,
+    memberImgUrl: '',
+  })
   const [bgColor, textColor, successText] =
-    propsData.recordStatus !== 2
+    data?.recordStatus !== 2
       ? ['bg-seesaw-blue-100', 'text-primary', '성공']
       : ['bg-seesaw-red-100', 'text-error', '실패']
-  const balance = recordStatus.missionTargetPrice - propsData.recordTotalCost
+  const balance = recordStatus.missionTargetPrice - data?.recordTotalCost
   let passTime = ''
 
-  const getRecordList = (recordNumber: number, list: RecordList[]) => {
-    if (list.length === 0) {
-      return
-    }
-    const targetIdx = list.findIndex(
-      (element) => element.recordNumber === recordNumber,
-    )
-    console.log('스토어확인', list)
-    return list[targetIdx].recordList
-  }
+  // const getRecordList = (recordNumber: number, list: RecordList[]) => {
+  //   if (list.length === 0) {
+  //     return
+  //   }
+  //   const targetIdx = list.findIndex(
+  //     (element) => element.recordNumber === recordNumber,
+  //   )
+  //   console.log('스토어확인', list)
+  //   return list[targetIdx].recordList
+  // }
 
   useEffect(() => {
-    passTime = getTimeBefore(propsData.recordWriteTime)
-  })
+    const init = async () => {
+      setRecordDetail(await data)
+      passTime = getTimeBefore(data?.recordWriteTime)
+    }
+    init()
+  }, [])
   return (
-    <div className="rounded-lg bg-background m-5 flex flex-col">
-      <div>
-        {/* 헤더 */}
+    <div className="rounded-lg bg-background m-5">
+      {/* 헤더 */}
+      <div className={`${bgColor} p-5`}>
         <div className="flex justify-between w-full mb-2.5">
           <span>
             <span className="font-scDreamMedium mr-[10px]">
-              {propsData.recordNumber}회차
+              {data && data.recordNumber}회차
             </span>
             <span className="text-[10px] text-outline">
-              {getCycleTerm(
-                recordStatus.missionStartDate,
-                propsData.recordNumber,
-                recordStatus.missionPeriod,
-              )}
+              {isSuccess &&
+                data &&
+                getCycleTerm(
+                  recordStatus.missionStartDate,
+                  data.recordNumber,
+                  recordStatus.missionPeriod,
+                )}
             </span>
           </span>
-          {memberEmail === propsData.memberEmail && (
+          {data && memberEmail === data.memberEmail && (
             <FontAwesomeIcon
               icon={faPenToSquare}
               className="text-outline"
-              onClick={() => router.push('/home')}
+              onClick={() => router.push(`${recordId}/create`)}
             />
           )}
         </div>
-        <div className={`collapse rounded-b-none`}>
+        <div className={`collapse rounded-t-lg rounded-b-none`}>
           <input
             type="checkbox"
             ref={checkRef}
@@ -92,59 +119,74 @@ const RecordContentContainer = ({
           {/* collapase title 간략 정보*/}
           <div className="collapse-title flex flex-col items-center p-0">
             {/* 기간 및 자세히 보기 */}
-          </div>
-          {/* 프로필 및 등록 시간 */}
-          <div className="flex justify-between items-center w-full mb-2.5">
-            <span>
-              <Image
-                src={propsData.memberImgUrl ?? '/default_profile.svg'}
-                width={35}
-                height={35}
-                alt="member profile image"
-                className="rounded-full inline-block mr-[15px] w-[38px] h-[38px] bg-white"
-              />
-              <span>{propsData.memberNickname}</span>
-            </span>
-            <span className="text-[10px] text-outline">{passTime}</span>
-          </div>
-          {/* 성공 여부 및 잔액 */}
-          <div className="w-full flex justify-between">
-            <span className={`font-scDreamExBold ${textColor}`}>
-              {successText}
-            </span>
-            <span>
-              <span className="text-[10px] text-outline mr-[10px]">잔액</span>
-              <span className={`font-scDreamExBold ${textColor}`}>
-                {propsData.recordTotalCost.toLocaleString('ko-KR')}
+
+            {/* 프로필 및 등록 시간 */}
+            <div className="flex justify-between items-center w-full mb-2.5">
+              <span>
+                <Image
+                  src={data?.memberImgUrl ?? '/default_profile.svg'}
+                  width={35}
+                  height={35}
+                  alt="member profile image"
+                  className="rounded-full inline-block mr-[15px] bg-white w-[35px] h-[35px]"
+                />
+                <span>{data && data.memberNickname}</span>
               </span>
-            </span>
+              <span className="text-[10px] text-outline">{passTime}</span>
+            </div>
+            {/* 성공 여부 및 잔액 */}
+            <div className="w-full flex justify-between">
+              <span className={`font-scDreamExBold ${textColor}`}>
+                {successText}
+              </span>
+              <span>
+                <span className="text-[10px] text-outline mr-[10px]">소비</span>
+                <span className={`font-scDreamExBold ${textColor}`}>
+                  {data && data.recordTotalCost.toLocaleString('ko-KR')}
+                </span>
+              </span>
+            </div>
+            <div className="w-full text-center">
+              <hr className="border-outline my-2.5 w-full" />
+              {!isOpened && <FontAwesomeIcon icon={faChevronDown} />}
+            </div>
           </div>
-          <div className="w-full text-center">
-            <hr className="border-outline my-2.5 w-full" />
-            {!isOpened && <FontAwesomeIcon icon={faChevronDown} />}
-          </div>
+          {/* collpase content - 거래 내역 상세 */}
+          <SpendingHistory
+            textColor={textColor}
+            targetPrice={recordStatus.missionTargetPrice}
+            history={spendingHistory}
+            balance={balance}
+          />
+          {isOpened && (
+            <FontAwesomeIcon
+              icon={faChevronUp}
+              className="mx-auto"
+              onClick={() => checkRef.current?.click()}
+            />
+          )}
         </div>
       </div>
-      {/* collpase content - 거래 내역 상세 */}
-      <SpendingHistory
-        textColor={textColor}
-        targetPrice={recordStatus.missionTargetPrice}
-        history={getRecordList(propsData.recordNumber, recordList)}
-        balance={balance}
-      />
-      {isOpened && (
-        <FontAwesomeIcon
-          icon={faChevronUp}
-          className="mx-auto"
-          onClick={() => checkRef.current?.click()}
-        />
-      )}
       {/* 본문 */}
+
       <div className="p-5">
-        {propsData.recordContent ?? '입력된 메모가 없습니다.'}
+        {(data && data.recordContent) ?? '입력된 메모가 없습니다.'}
       </div>
     </div>
   )
+}
+
+const getRecordContent = async (recordId: number) => {
+  console.log('이거왜 언디파인드', recordId)
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_SEESAW_API_URL}/record/${recordId}`,
+    {
+      method: 'get',
+    },
+  ).then((res) => {
+    let s = res.json()
+    return s
+  })
 }
 
 export default RecordContentContainer
--- front/app/(mission)/mission/[missionId]/[recordId]/page.tsx
+++ front/app/(mission)/mission/[missionId]/[recordId]/page.tsx
@@ -3,20 +3,40 @@ import RecordContentContainer from './components/RecordContentContainer'
 import CommentInput from './components/CommentInput'
 
 const RecordIdPage = async ({ params }: { params: any }) => {
-  const recordDetail = await getRecordContent(params.recordId)
-  console.log(recordDetail, params.recordId)
+  // const [recordDetail, spendingHistory] = await Promise.all([
+  //   getRecordContent(params.recordId),
+  //   getSpendingHistory(params.recordId),
+  // ])
+  const spendingHistory = await getSpendingHistory(params.recordId)
+
   return (
     <div className="bg-background-fill pt-[68px] min-h-[844px]">
-      <RecordContentContainer propsData={recordDetail} />
+      <RecordContentContainer
+        recordId={params.recordId}
+        spendingHistory={spendingHistory}
+      />
       <CommentsContainer recordId={params.recordId} />
       <CommentInput recordId={params.recordId} />
     </div>
   )
 }
-const getRecordContent = async (recordId: number) => {
-  console.log('이거왜 언디파인드', recordId)
+
+// const getRecordContent = async (recordId: number) => {
+//   console.log('이거왜 언디파인드', recordId)
+//   return await fetch(
+//     `${process.env.NEXT_PUBLIC_SEESAW_API_URL}/record/${recordId}`,
+//     {
+//       method: 'get',
+//     },
+//   ).then((res) => {
+//     let s = res.json()
+//     return s
+//   })
+// }
+
+const getSpendingHistory = async (recordId: number) => {
   return await fetch(
-    `${process.env.NEXT_PUBLIC_SEESAW_API_URL}/record/${recordId}`,
+    `${process.env.NEXT_PUBLIC_SEESAW_API_URL}/spending/record/${recordId}`,
     {
       method: 'get',
     },
