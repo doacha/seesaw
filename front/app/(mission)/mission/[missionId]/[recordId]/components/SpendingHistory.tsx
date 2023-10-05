450b26f1fb294c9e5572b253742b399dfeec1e63
김봉준 <zune1408@gmail.com>
2023-10-05 16:52:14 +0900

    :bug: Fix: FE/레코드갱신문제 - next server component의 fetch를 revalidate할 수 가 없어, spendingList를 client component에서 fetch 요청

--- front/app/(mission)/mission/[missionId]/[recordId]/components/SpendingHistory.tsx
+++ front/app/(mission)/mission/[missionId]/[recordId]/components/SpendingHistory.tsx
@@ -1,10 +1,11 @@
 import { RecordList } from '@/app/types'
-
+import { useQuery } from '@tanstack/react-query'
 const SpendingHistory = ({
   history,
   targetPrice,
   balance,
   textColor,
+  recordId,
 }: {
   history:
     | Array<{
@@ -15,21 +16,30 @@ const SpendingHistory = ({
   targetPrice: number
   balance: number
   textColor: string
+  recordId: number
 }) => {
+  const { data } = useQuery({
+    queryKey: ['spendingHistory', recordId],
+    queryFn: () => getSpendingHistory(recordId),
+    staleTime: 50000,
+  })
+
   return (
     <div className="collapse-content p-0">
       <div className="flex justify-between mb-5">
         <span className="font-scDreamExBold">미션금액</span>
         <span>{targetPrice.toLocaleString('ko-KR')}</span>
       </div>
-      {history &&
-        history.length > 0 &&
-        history.map((element, idx) => (
-          <div className="flex justify-between mb-2" key={idx}>
-            <span>{element.spendingTitle}</span>
-            <span>{element.spendingCost.toLocaleString('ko-KR')}</span>
-          </div>
-        ))}
+      {data &&
+        data.length > 0 &&
+        (data as Array<{ spendingTitle: string; spendingCost: number }>).map(
+          (element, idx) => (
+            <div className="flex justify-between mb-2" key={idx}>
+              <span>{element.spendingTitle}</span>
+              <span>{element.spendingCost.toLocaleString('ko-KR')}</span>
+            </div>
+          ),
+        )}
       {history && history.length === 0 && (
         <div className="text-center mb-2.5">거래 내역이 없습니다.</div>
       )}
@@ -42,4 +52,15 @@ const SpendingHistory = ({
   )
 }
 
+const getSpendingHistory = async (recordId: number) => {
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_SEESAW_API_URL}/spending/record/${recordId}`,
+    {
+      method: 'get',
+    },
+  ).then((res) => {
+    let s = res.json()
+    return s
+  })
+}
 export default SpendingHistory
